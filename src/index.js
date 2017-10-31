7da00e48d2ba41e88d66e52511f4be26be973fc9
theperfectfuel <robert@theperfectfuel.com>
2017-10-31 01:36:09 -0700

    done setting up youtube api search npm package

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import config from '../config';
+import ytsearch from 'youtube-api-search';
 
 import Searchbar from './components/Searchbar';
 
-const App = () => {
-    return (
-        <div>
-            <Searchbar />
-        </div>
-    );
+const YT_API_KEY = config.YT_API_KEY;
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: []
+        };
+
+        ytsearch({ key: YT_API_KEY, term: 'drums' }, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Searchbar />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.getElementById('container'));
